09e33f44498a4c0661da16f39f79cbdd23105593
prljav <prljav@prljav.cc>
2025-01-03 23:14:30 +0100

    fix(build): fixes build output files being called main instead of index

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/main.ts"],
+    entry: { index: "src/main.ts" },
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
     splitting: false,
