3b3837ae88edb31156e0e34341f778c674f3a25c
prljav <prljav@prljav.cc>
2025-01-05 07:43:58 +0100

    fix: fixes formatting and displays the correct username in the debug connection

--- src/bot.ts
+++ src/bot.ts
@@ -7,119 +7,119 @@ import { removeAnsiCodes } from "./util";
 const PR_WS = "wss://play.proceduralrealms.com/ws";
 
 export class BotBase extends EventManager {
-	websocket: WebSocket;
-	websocketServer: WebSocketServer;
+  websocket: WebSocket;
+  websocketServer: WebSocketServer;
 
-	startTime = 0;
-	protected isReady = false;
+  startTime = 0;
+  protected isReady = false;
 
-	options: BotOptions;
-	protected botState: BotState;
+  options: BotOptions;
+  protected botState: BotState;
 
-	constructor(options: BotOptions) {
-		super();
-		this.botState = {
-			x: undefined,
-			y: undefined,
-			region: undefined,
-			map: undefined,
-		};
+  constructor(options: BotOptions) {
+    super();
+    this.botState = {
+      x: undefined,
+      y: undefined,
+      region: undefined,
+      map: undefined,
+    };
 
-		this.options = options;
-		this.websocket = new WebSocket(PR_WS);
+    this.options = options;
+    this.websocket = new WebSocket(PR_WS);
 
-		if (this.options.debug === true) {
-			this.runInDebugMode();
-		}
+    if (this.options.debug === true) {
+      this.runInDebugMode();
+    }
 
-		// Initialize the WebSocket connection
-		this.websocket.on("open", () => {
-			this.websocket.send(options.auth);
-			this.startTime = Date.now();
-		});
+    // Initialize the WebSocket connection
+    this.websocket.on("open", () => {
+      this.websocket.send(options.auth);
+      this.startTime = Date.now();
+    });
 
-		this.websocket.on("message", (data: string) => {
-			this.parsePacket(JSON.parse(data));
-		});
-	}
+    this.websocket.on("message", (data: string) => {
+      this.parsePacket(JSON.parse(data));
+    });
+  }
 
-	runInDebugMode() {
-		console.info("Running in debug mode");
-		this.websocketServer = new WebSocketServer({
-			port: 8008,
-			host: "127.0.0.1",
-		});
-		this.websocketServer.on("connection", (client) => {
-			console.log("client connected");
-			client.on("message", (_data: unknown) => {
-				const data = _data.toString();
-				const reqId = JSON.parse(data).reqId;
-				if (JSON.parse(data).cmd === "token") {
-					return setTimeout(() => {
-						client.send(
-							`{"cmd":"token.success","msg":{"name":"Prljav","token":"sadsads"},"reqId":"${reqId}"}`,
-						);
-					}, 500);
-				}
-				this.websocket.send(data, { binary: false });
-			});
-		});
-	}
+  runInDebugMode() {
+    console.info("Running in debug mode");
+    this.websocketServer = new WebSocketServer({
+      port: 8008,
+      host: "127.0.0.1",
+    });
+    this.websocketServer.on("connection", (client) => {
+      console.log("client connected");
+      client.on("message", (_data: unknown) => {
+        const data = _data.toString();
+        const reqId = JSON.parse(data).reqId;
+        if (JSON.parse(data).cmd === "token") {
+          return setTimeout(() => {
+            client.send(
+              `{"cmd":"token.success","msg":{"name":"${JSON.parse(data).msg.name}","token":"sadsads"},"reqId":"${reqId}"}`,
+            );
+          }, 500);
+        }
+        this.websocket.send(data, { binary: false });
+      });
+    });
+  }
 
-	quit() {
-		this.websocket.close();
-	}
+  quit() {
+    this.websocket.close();
+  }
 
-	runCmd(cmd: string) {
-		this.websocket.send(JSON.stringify({ cmd: "cmd", msg: cmd }));
-	}
+  runCmd(cmd: string) {
+    this.websocket.send(JSON.stringify({ cmd: "cmd", msg: cmd }));
+  }
 
-	parsePacket(packet: PacketType) {
-		//parsing for the mitm ws server
-		if (this.options.debug === true) {
-			this.websocketServer?.clients.forEach((client) => {
-				client.send(JSON.stringify(packet));
-			});
-		}
-		//end
+  parsePacket(packet: PacketType) {
+    //parsing for the mitm ws server
+    if (this.options.debug === true) {
+      this.websocketServer?.clients.forEach((client) => {
+        client.send(JSON.stringify(packet));
+      });
+    }
+    //end
 
-		if ("cmd" in packet && packet.cmd === "room.describe") {
-			try {
-				this.botState.map = (packet.msg as { map: unknown[] }).map.join("\n");
+    if ("cmd" in packet && packet.cmd === "room.describe") {
+      try {
+        this.botState.map = (packet.msg as { map: unknown[] }).map.join("\n");
 
-				const desc = (packet.msg as { desc: string }).desc;
-				const cleanedDesc = removeAnsiCodes(desc);
-				this.botState.region = cleanedDesc.split(" | ")[1].trim();
-				const numbers = cleanedDesc.match(/(?<=[ ,])\d+(?=[ ,])/g);
-				this.botState.x = Number(numbers[0]);
-				this.botState.y = Number(numbers[1]);
-				this.dispatchEvent("update", this.botState);
-			} catch (e) {
-				console.warn(`Failed to update bot state, ${e}`);
-			}
-		}
+        const desc = (packet.msg as { desc: string }).desc;
+        const cleanedDesc = removeAnsiCodes(desc);
+        this.botState.region = cleanedDesc.split(" | ")[1].trim();
+        const numbers = cleanedDesc.match(/(?<=[ ,])\d+(?=[ ,])/g);
+        this.botState.x = Number(numbers[0]);
+        this.botState.y = Number(numbers[1]);
+        this.dispatchEvent("update", this.botState);
+      } catch (e) {
+        console.warn(`Failed to update bot state, ${e}`);
+      }
+    }
 
-		if (!("cmd" in packet)) return;
+    if (!("cmd" in packet)) return;
 
-		if (packet.cmd === "token.success") {
-			this.isReady = true;
-			this.dispatchEvent("ready", packet);
-		}
+    if (packet.cmd === "token.success") {
+      this.isReady = true;
+      this.dispatchEvent("ready", packet);
+    }
 
-		if (this.options.events?.all) {
-			this.options.events.all(packet);
-		}
+    if (this.options.events?.all) {
+      this.options.events.all(packet);
+    }
 
-		if (this.options.events[packet.cmd as string]) {
-			this.options.events[packet.cmd as string](packet);
-		}
-	}
+    if (this.options.events[packet.cmd as string]) {
+      this.options.events[packet.cmd as string](packet);
+    }
+  }
 
-	sendToClient(msg: string) {
-		if (this.options.debug !== true)
-			throw "Not in debug mode, cannot use sendToClient";
-		this.websocketServer?.clients.forEach((client) => {
-			client.send(msg);
-		});
-	}
+  sendToClient(msg: string) {
+    if (this.options.debug !== true)
+      throw "Not in debug mode, cannot use sendToClient";
+    this.websocketServer?.clients.forEach((client) => {
+      client.send(msg);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { BotBase } from "./bot";
-
-export class Bot extends BotBase {
-	get botstate() {
-		return this.botState;
-	}
-	get ws() {
-		return this.websocket;
-	}
-	get wss() {
-		return this.websocketServer;
-	}
-	get ready() {
-		return this.isReady;
-	}
+import { BotOptions } from "./types";
+import { PacketType } from "./types";
+import { BotState } from "./types";
+import { removeAnsiCodes } from "./util";
+class Bot extends BotBase {
+  get botstate() {
+    return this.botState;
+  }
+  get ws() {
+    return this.websocket;
+  }
+  get wss() {
+    return this.websocketServer;
+  }
+  get ready() {
+    return this.isReady;
+  }
 }
+
+export { Bot, BotOptions, PacketType, BotState, removeAnsiCodes }
