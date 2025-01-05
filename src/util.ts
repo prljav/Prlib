ad9c87b70e421a4245a525ea3bfd39281cb8a389
JagTheFriend <JagTheFriend12@gmail.com>
2025-01-05 10:57:00 +0530

    fix: :bug: The removeAnsiCodes should work and not break randomly
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,15 @@
-//TODO: make sure this actually works and doesnt break randomly
+function ansiRegex() {
+	// Valid string terminator sequences are BEL, ESC\, and 0x9c
+	const ST = "(?:\\u0007|\\u001B\\u005C|\\u009C)";
+	const pattern = [
+		`[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?${ST})`,
+		"(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))",
+	].join("|");
+
+	return new RegExp(pattern, "g");
+}
+
 export function removeAnsiCodes(input: string) {
-    const ansiRegex = /\u001b\[[0-9;]*[a-zA-Z]/g;
-    return input.replace(ansiRegex, '');
+	const regex = ansiRegex();
+	return input.replace(regex, "");
 }
